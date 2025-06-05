9be7e73ae70ba0b350c23f523b15f06944ba5554
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-06-05 10:47:15 +0500

    TEL-51: - added sample config files - added database support

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"attendance-telegram-bot/internal/database"
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+	"log"
+)
+
 func main() {
+	if err := InitConfig(); err != nil {
+		log.Fatal("InitConfig(): error loading config.yaml", err)
+	}
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error loading .env file", err)
+	}
+	conn := database.NewConnectPostgres()
+	repositories := database.NewDatabase(conn)
+	log.Println(repositories) // delete me
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("internal/configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
